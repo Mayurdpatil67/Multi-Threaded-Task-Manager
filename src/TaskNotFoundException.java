5ed4bddc0a12fd55ab6da6b08223a3313830cafa
Mayurdpatil67 <mayurpatil096690@gmail.com>
2025-01-09 22:53:04 +0530

    created custom excption

---
+++ src/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+public class TaskNotFoundException extends Exception {
+
+    public TaskNotFoundException(String message) {
+        super(message); // passing the exception message to superclass (Exception)
+    }
+
+}
