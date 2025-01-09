2e0fe13f02fe147c610ea164174c67c97037a1d2
Mayurdpatil67 <mayurpatil096690@gmail.com>
2025-01-09 22:41:17 +0530

    getting started with task class

---
+++ src/Task.java
@@ -0,0 +1,40 @@
+public class Task {
+
+    private String title;
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.isCompleted = false; // initialising the task as not completed
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    // method to mark the task as completed
+    public void markCompleted() {
+        this.isCompleted = true;
+    }
+
+    // overriding toString method to print task details in formated way
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+
+}
